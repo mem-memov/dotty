4afe62e7f6619248f9ef6b4b60d7c84b2052d169
Alexander Novikov <mem-memov@yandex.ru>
2019-07-20 01:58:34 +0300

    algebra

--- dotty/src/main/scala/Main.scala
+++ dotty/src/main/scala/Main.scala
@@ -1,14 +1,20 @@
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
-    println(msg)
+
+    import vo._
+    val у1 = Утверждение(None, Map(Вопрос("где") -> Some(Ответ("низ"))))
+    val у2 = Утверждение(None, Map(Вопрос("кто") -> Some(Ответ("собака"))))
+    val у3 = Утверждение(None, Map(Вопрос("когда") -> Some(Ответ("ночь"))))
+    val у4 = Утверждение(None, Map(Вопрос("зачем") -> None))
+    val у = Утверждение(Some(Действие("лаять")))
+    
+    println(у)
+    println(у + у1 + у2 + у3)
+    println(у + у1 + у2 + у4)
+    println(у + у1 + у2 - у1)
+    println(у - у)
+    println(у1 - (у + у1 + у2))
+
   }
-
-  def msg = "I was compiled by dotty :)"
-
-  enum Color {
-    case Red, Green, Yellow
-  }
-
 }
